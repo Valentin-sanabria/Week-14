98f3e776c1db8b2911397433c56b6ef1e5e42422
Valentin-sanabria <valensanabria2000@hotmail.com>
2022-11-19 15:47:10 -0300

    TG-14: Initial tests for each user.

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -11,4 +11,52 @@ describe('My Login application', () => {
             'You logged into a secure area!');
     }); */
 
+    it('Log in with standard_user',  async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user','secret_sauce');
+    })
+
+    it('Log out with standard_user',  async () => {
+        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
+        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
+        await sideBarBurger.click();
+        await logoutBtn.waitForClickable();
+        await logoutBtn.click();
+    })
+
+    it('Log in with locked_out_user',  async () => {
+        const errorMsg = $('//*[@id="login_button_container"]/div/form/div[3]/h3');
+        const closeErrorBtn = $('//*[@id="login_button_container"]/div/form/div[3]/h3/button');
+
+        await LoginPage.open();
+        await LoginPage.login('locked_out_user','secret_sauce');
+        await expect(errorMsg).toHaveTextContaining('Epic sadface: Sorry, this user has been locked out.');
+        await closeErrorBtn.click();
+    })
+
+    it('Log in with performance_glitch_user',  async () => {
+        await LoginPage.open();
+        await LoginPage.login('performance_glitch_user','secret_sauce');
+    })
+
+    it('Log out with performance_glitch_user',  async () => {
+        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
+        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
+        await sideBarBurger.click();
+        await logoutBtn.waitForClickable();
+        await logoutBtn.click();
+    })
+
+    it('Log in with problem_user',  async () => {
+        await LoginPage.open();
+        await LoginPage.login('problem_user','secret_sauce');
+    })
+
+    it('Log out with problem_user',  async () => {
+        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
+        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
+        await sideBarBurger.click();
+        await logoutBtn.waitForClickable();
+        await logoutBtn.click();
+    })
 });
