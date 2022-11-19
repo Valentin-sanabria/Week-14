47adbb64da49c37b792219618981c8c10fce957b
Valentin-sanabria <valensanabria2000@hotmail.com>
2022-11-19 17:18:29 -0300

    TG-14: Replace xPath with selector.

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,39 +1,39 @@
+class LoginPage {
 
-
-import Page from './page';
-
-/**
- * sub page containing specific selectors and methods for a specific page
- */
-class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     get inputUsername () {
-        return $('//*[@id="user-name"]');
+        return $('#user-name');
     }
 
     get inputPassword () {
-        return $('//*[@id="password"]');
+        return $('#password');
     }
 
-    get btnSubmit () {
-        return $('//*[@id="login-button"]');
+    get btnLogin () {
+        return $('#login-button');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    get sideBarBurger () {
+        return $('#react-burger-menu-btn');
+    }
+
+    get logoutBtn () {
+        return $('#logout_sidebar_link');
+    }
+
+    get errorMsg () {
+        return $('#login_button_container > div > form > div.error-message-container.error');
+    }
+
+    get closeErrorBtn () {
+        return $('#login_button_container > div > form > div.error-message-container.error > h3 > button');
+    }
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+        await this.btnLogin.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     open () {
         return super.open('login');
     }
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,7 +1,9 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
 
 describe('My Login application', () => {
+    beforeAll('Open webpage with url', () => {
+        browser.url("https://www.saucedemo.com/")
+    })
    /* it('should login with valid credentials', async () => {
         await LoginPage.open();
 
@@ -12,51 +14,38 @@ describe('My Login application', () => {
     }); */
 
     it('Log in with standard_user',  async () => {
-        await LoginPage.open();
         await LoginPage.login('standard_user','secret_sauce');
     })
 
     it('Log out with standard_user',  async () => {
-        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
-        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
-        await sideBarBurger.click();
-        await logoutBtn.waitForClickable();
-        await logoutBtn.click();
+        await LoginPage.sideBarBurger.click();
+        await LoginPage.logoutBtn.waitForClickable();
+        await LoginPage.logoutBtn.click();
     })
 
-    it('Log in with locked_out_user',  async () => {
-        const errorMsg = $('//*[@id="login_button_container"]/div/form/div[3]/h3');
-        const closeErrorBtn = $('//*[@id="login_button_container"]/div/form/div[3]/h3/button');
-
-        await LoginPage.open();
+    it('Try to log in with locked_out_user',  async () => {
         await LoginPage.login('locked_out_user','secret_sauce');
-        await expect(errorMsg).toHaveTextContaining('Epic sadface: Sorry, this user has been locked out.');
-        await closeErrorBtn.click();
+        await expect(LoginPage.errorMsg).toHaveTextContaining('Epic sadface: Sorry, this user has been locked out.');
+        await LoginPage.closeErrorBtn.click();
     })
 
     it('Log in with performance_glitch_user',  async () => {
-        await LoginPage.open();
         await LoginPage.login('performance_glitch_user','secret_sauce');
     })
 
     it('Log out with performance_glitch_user',  async () => {
-        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
-        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
-        await sideBarBurger.click();
-        await logoutBtn.waitForClickable();
-        await logoutBtn.click();
+        await LoginPage.sideBarBurger.click();
+        await LoginPage.logoutBtn.waitForClickable();
+        await LoginPage.logoutBtn.click();
     })
 
     it('Log in with problem_user',  async () => {
-        await LoginPage.open();
         await LoginPage.login('problem_user','secret_sauce');
     })
 
     it('Log out with problem_user',  async () => {
-        const sideBarBurger = $('//*[@id="react-burger-menu-btn"]');
-        const logoutBtn = $('//*[@id="logout_sidebar_link"]');
-        await sideBarBurger.click();
-        await logoutBtn.waitForClickable();
-        await logoutBtn.click();
+        await LoginPage.sideBarBurger.click();
+        await LoginPage.logoutBtn.waitForClickable();
+        await LoginPage.logoutBtn.click();
     })
 });
