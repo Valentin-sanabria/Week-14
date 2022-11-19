ec76db89c70d8ac28243e37f542573af3effb227
Valentin-sanabria <valensanabria2000@hotmail.com>
2022-11-19 15:13:10 -0300

    TG-14: Instalation of wdio.

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,42 @@
+
+
+import Page from './page';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () {
+        return $('//*[@id="user-name"]');
+    }
+
+    get inputPassword () {
+        return $('//*[@id="password"]');
+    }
+
+    get btnSubmit () {
+        return $('//*[@id="login-button"]');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
+    }
+}
+
+export default new LoginPage();
